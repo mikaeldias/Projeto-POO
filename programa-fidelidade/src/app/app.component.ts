522b2d4a4beba9b41e84fab2a19726d4bf1414f4
mikaeldias <165174397+MikaelDiasS10@users.noreply.github.com>
2025-02-21 15:36:08 -0300

    Criação de componentes bases

--- programa-fidelidade/src/app/app.component.ts
+++ programa-fidelidade/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./navbar/navbar.component";
+import { HomeComponent } from "./home/home.component";
+import { FooterComponent } from "./footer/footer.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, HomeComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
